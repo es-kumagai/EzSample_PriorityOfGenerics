1c56e290abfb05b228c1197b620d0827140b503c
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-09-08 22:52:37 +0900

    Initial Commit

---
+++ EzSample_PriorityOfGenerics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EzSample_PriorityOfGenerics
+//
+//  Created by Tomohiro Kumagai on H27/09/08.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
